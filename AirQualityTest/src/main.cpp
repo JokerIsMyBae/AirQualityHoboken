5eb5db3f9447a67ac1d89fcdd9510a5b95e98339
Joos Van Esbroeck <joos.van.esbroeck@telenet.be>
2023-09-28 11:52:27 +0200

    feat: created sen55 example usage

--- AirQualityTest/src/main.cpp
+++ AirQualityTest/src/main.cpp
@@ -1,20 +1,61 @@
 #include <Arduino.h>
 #include <SensirionI2CSen5x.h>
+#include <Wire.h>
 
+SensirionI2CSen5x sen55;
 
-// put function declarations here: 
-int myFunction(int, int);
+uint16_t error;
+char errorMsg[256];
+bool dataReady = false;
+
+float massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, 
+massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex, noxIndex;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  Serial.begin(115200);
+
+  Wire.begin();
+  sen55.begin(Wire);
+
 }
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  error = sen55.startMeasurement();
+  if (error) {
+    Serial.print("Error trying to execute startMeasurement(): ");
+    errorToString(error, errorMsg, 256);
+    Serial.println(errorMsg);
+  }
+  
+  do {
+    error = sen55.readDataReady(dataReady);
+    if (error) {
+      Serial.print("Error trying to read dataReady flag: ");
+      errorToString(error, errorMsg, 256);
+      Serial.println(errorMsg);
+    }
+    delay(1);
+  } while (!dataReady);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  error = sen55.readMeasuredValues(
+    massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0,
+    massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex,
+    noxIndex
+    );
+  if (error) {
+    Serial.print("Error trying to execute readMeasuredValues(): ");
+    errorToString(error, errorMsg, 256);
+    Serial.println(errorMsg);
+  }
+
+  error = sen55.stopMeasurement();
+  if (error) {
+    Serial.print("Error returning sensor to idle: ");
+    errorToString(error, errorMsg, 256);
+    Serial.println(errorMsg);
+  }
+
 }
