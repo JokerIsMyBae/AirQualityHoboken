ae43a0c985db744747a66287da9348ad6840823e
Joos Van Esbroeck <joos.van.esbroeck@telenet.be>
2023-11-23 16:42:30 +0100

    fix: fix everything and add otaa

--- sensor-code/Sensor-LoRa-Sleep/src/main.cpp
+++ sensor-code/Sensor-LoRa-Sleep/src/main.cpp
@@ -2,29 +2,30 @@
 #include <Wire.h>
 
 #include <SensirionI2CSen5x.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
 
-#include <configuration.h>
-#include <sleep.h>
-#include <ttn.h>
+#include "configuration.h"
+#include "credentials.h"
+
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+static uint8_t txBuffer[DATA_LENGTH];
+static osjob_t sendjob;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = NSS_GPIO,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = RESET_GPIO,
+    .dio = {DIO0_GPIO, DIO1_GPIO, DIO2_GPIO},
+};
 
 SensirionI2CSen5x sen55;
-
-bool pmu_irq = false;
-String baChStatus = "No charging";
-
-bool packetSent, packetQueued;
-static uint8_t txBuffer[DATA_LENGTH];
 float f_pm1p0, f_pm2p5, f_pm4p0, f_pm10p0, f_hum, f_temp, f_voc, f_nox;
-
-/**
-   If we have a valid position send it to the server.
-   @return true if we decided to send.
-*/
-bool try_send() {
-  packetSent = false;
-  ttn_send(txBuffer, DATA_LENGTH, LORAWAN_PORT, false);
-  return true;
-}
 
 // void doDeepSleep(uint64_t secToWake)
 // {
@@ -36,166 +37,171 @@ bool try_send() {
 //   sleep_seconds(secToWake); 
 // }
 
-void callback(uint8_t message) {
-  bool ttn_joined = false;
-  if (EV_JOINED == message) {
-    ttn_joined = true;
-  }
-
-  // We only want to say 'packetSent' for our packets (not packets needed for joining)
-  if (EV_TXCOMPLETE == message && packetQueued) {
-    packetQueued = false;
-    packetSent = true;
-  }
-
-  if (EV_RESPONSE == message) {
-    size_t len = ttn_response_len();
-    uint8_t data[len];
-    ttn_response(data, len);
-
-    char buffer[6];
-    for (uint8_t i = 0; i < len; i++) {
-      snprintf(buffer, sizeof(buffer), "%02X", data[i]);
-    }
-  }
+void printHex2(unsigned v) {
+  v &= 0xff;
+  if (v < 16)
+    Serial.print('0');
+  Serial.print(v, HEX);
 }
 
-uint16_t read_measurements(SensirionI2CSen5x &sen55, float &pm1p0, float &pm2p5, float &pm4p0, 
-  float &pm10p0, float &hum, float &temp, float &voc, float &nox) {
+void do_send(osjob_t* j){
+  sen55.readMeasuredValues(
+    f_pm1p0, f_pm2p5, f_pm4p0, f_pm10p0, f_hum, f_temp, f_voc, f_nox
+  );
 
-  uint16_t error = 0x0000;
+  uint32_t u_pm1p0 = f_pm1p0 * 100;
+  uint32_t u_pm2p5 = f_pm2p5 * 100;
+  uint32_t u_pm4p0 = f_pm4p0 * 100;
+  uint32_t u_pm10p0 = f_pm10p0 * 100;
+  uint32_t u_hum = f_hum * 100;
+  uint32_t u_temp = (f_temp + 10) * 100;
+  uint32_t u_voc = f_voc * 100;
+  uint32_t u_nox = f_nox * 100;
 
-  error = sen55.readMeasuredValues(pm1p0, pm2p5, pm4p0, pm10p0, hum, temp, voc, nox);
-  if (error) 
-    return error;
-  else
-    return sen55.stopMeasurement();
+  uint32_t data[8] = { u_pm1p0, u_pm2p5, u_pm4p0, u_pm10p0, u_hum, u_temp, u_voc, u_nox };
+
+  for (byte i = 0; i < 8; i++) {
+    txBuffer[0+i*2] = (data[i] >> 8) & 0xFF;
+    txBuffer[1+i*2] = (data[i]) & 0xFF;
+  }
+
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, txBuffer, DATA_LENGTH, 0);
+    Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent (ev_t ev) {
+  switch(ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      {
+        u4_t netid = 0;
+        devaddr_t devaddr = 0;
+        u1_t nwkKey[16];
+        u1_t artKey[16];
+        LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+        Serial.print("netid: ");
+        Serial.println(netid, DEC);
+        Serial.print("devaddr: ");
+        Serial.println(devaddr, HEX);
+        Serial.print("AppSKey: ");
+        for (size_t i=0; i<sizeof(artKey); ++i) {
+          if (i != 0)
+            Serial.print("-");
+          printHex2(artKey[i]);
+        }
+        Serial.println("");
+        Serial.print("NwkSKey: ");
+        for (size_t i=0; i<sizeof(nwkKey); ++i) {
+                if (i != 0)
+                        Serial.print("-");
+                printHex2(nwkKey[i]);
+        }
+        Serial.println();
+      }
+      // Disable link check validation (automatically enabled
+      // during join, but because slow data rates change max TX
+      // size, we don't use it in this example.
+      LMIC_setLinkCheckMode(0);
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if (LMIC.txrxFlags & TXRX_ACK)
+        Serial.println(F("Received ack"));
+      if (LMIC.dataLen) {
+        Serial.print(F("Received "));
+        Serial.print(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    case EV_TXSTART:
+      Serial.println(F("EV_TXSTART"));
+      break;
+    case EV_TXCANCELED:
+      Serial.println(F("EV_TXCANCELED"));
+      break;
+    case EV_RXSTART:
+      /* do not print anything -- it wrecks timing */
+      break;
+    case EV_JOIN_TXCOMPLETE:
+      Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+      break;
+
+    default:
+      Serial.print(F("Unknown event: "));
+      Serial.println((unsigned) ev);
+      break;
+  }
 }
 
 void setup() {
+
   Serial.begin(SERIAL_BAUD);
-  while (!Serial) {
-    delay(100);
-  }
+  while (!Serial);
+  Serial.println(F("Starting"));
 
   Wire.begin();
   sen55.begin(Wire);
+  sen55.startMeasurement();
 
+  SPI.begin(SCK_GPIO, MISO_GPIO, MOSI_GPIO, NSS_GPIO);
 
-  // TTN setup
-  if (!ttn_setup()) {
-    if (REQUIRE_RADIO) {
-      delay(MESSAGE_TO_SLEEP_DELAY);
-      sleep_forever();
-    }
-  }
-  else {
-    ttn_register(callback);
-    ttn_join();
-    ttn_adr(LORAWAN_ADR);
-  }
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  LMIC_setLinkCheckMode(0);
+  LMIC_setDrTxpow(LORAWAN_SF, 14);
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
 }
 
-
 void loop() {
-  uint16_t error = 0x0000;
-  char errorMsg[256];
-
-  //Serial.print("Starting measurements...");
-  error = sen55.startMeasurement();
-
-  if (!error) {
-    // SLEEP MCU FOR 2.5 TO 3 MINUTES FOR RELIABLE MEASUREMENTS
-    delay(120000);
-
-    error = read_measurements(
-      sen55, f_pm1p0, f_pm2p5, f_pm4p0, f_pm10p0, f_hum, f_temp, f_voc, f_nox
-      );
-  }
-  
-  /* if (error) {
-    Serial.print("Error trying to execute measurements: ");
-    errorToString(error, errorMsg, 256);
-    Serial.println(errorMsg);
-  }
-  else {
-    Serial.print("MassConcentrationPm1p0:");
-    Serial.print(f_pm1p0);
-    Serial.print("\t");
-    Serial.print("MassConcentrationPm2p5:");
-    Serial.print(f_pm2p5);
-    Serial.print("\t");
-    Serial.print("MassConcentrationPm4p0:");
-    Serial.print(f_pm4p0);
-    Serial.print("\t");
-    Serial.print("MassConcentrationPm10p0:");
-    Serial.print(f_pm10p0);
-    Serial.print("\t");
-    Serial.print("AmbientHumidity:");
-    if (isnan(f_hum)) 
-      Serial.print("n/a");
-    else 
-      Serial.print(f_hum);
-    Serial.print("\t");
-    Serial.print("AmbientTemperature:");
-    if (isnan(f_temp)) 
-      Serial.print("n/a");
-    else 
-      Serial.print(f_temp);
-    Serial.print("\t");
-    Serial.print("VocIndex:");
-    if (isnan(f_voc))
-      Serial.print("n/a");
-    else
-      Serial.print(f_voc);
-    Serial.print("\t");
-    Serial.print("NoxIndex:");
-    if (isnan(f_nox))
-      Serial.println("n/a");
-    else
-      Serial.println(f_nox); */
-
-    uint32_t u_pm1p0 = f_pm1p0 * 100;
-    uint32_t u_pm2p5 = f_pm2p5 * 100;
-    uint32_t u_pm4p0 = f_pm4p0 * 100;
-    uint32_t u_pm10p0 = f_pm10p0 * 100;
-    uint32_t u_hum = f_hum * 100;
-    uint32_t u_temp = (f_temp + 10) * 100;
-    uint32_t u_voc = f_voc * 100;
-    uint32_t u_nox = f_nox * 100;
-
-    uint32_t data[8] = { u_pm1p0, u_pm2p5, u_pm4p0, u_pm10p0, u_hum, u_temp, u_voc, u_nox };
-
-    for (byte i = 0; i < 8; i++) {
-      txBuffer[0+i*2] = (data[i] >> 8) & 0xFF;
-      txBuffer[1+i*2] = (data[i]) & 0xFF;
-    }
-
-    /* for (byte i = 0; i < 16; i++) {
-      Serial.print(txBuffer[i]);
-      Serial.print("\t");
-    } */
-
-    ttn_loop();
-
-    if (packetSent) 
-      packetSent = false;
-
-    // Send every SEND_INTERVAL millis
-    static uint32_t last = 0;
-    static bool first = true;
-    if (last == 0 || millis() - last > SEND_INTERVAL) {
-      if (try_send()) {
-        last = millis();
-        first = false;
-        //Serial.println("TRANSMITTED");
-      } else {
-        if (first) 
-          first = false;
-        // let the OS put the main CPU in low power mode for 100ms (or until another interrupt comes in)
-        // i.e. don't just keep spinning in loop as fast as we can.
-        delay(100);
-      }
-    }
-  }
-//}
+    os_runloop_once();
+}
