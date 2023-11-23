d793fd7c5d0224b1704a7d5ea64c1595cda01b77
Joos Van Esbroeck <joos.van.esbroeck@telenet.be>
2023-11-23 16:30:47 +0100

    feat: add config file

---
+++ sensor-code/Sensor-LoRa-Sleep/src/configuration.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Arduino.h>
+#include <lmic.h>
+
+// -----------------------------------------------------------------------------
+// Configuration
+// -----------------------------------------------------------------------------
+
+#define SERIAL_BAUD             115200          // Serial debug baud rate
+#define TX_INTERVAL             60              // Seconds between each transmit
+#define LORAWAN_PORT            1               // Port the messages will be sent to
+#define LORAWAN_SF              DR_SF10         // Spreading factor (recommended DR_SF7 for ttn network map purposes, DR_SF10 works for slow moving trackers)
+#define DATA_LENGTH             16              // Size of TX buffer sent over LoRa
+
+// -----------------------------------------------------------------------------
+// LoRa SPI
+// -----------------------------------------------------------------------------
+
+#define SCK_GPIO        5
+#define MISO_GPIO       19
+#define MOSI_GPIO       27
+#define NSS_GPIO        18
+#define RESET_GPIO      14
+#define DIO0_GPIO       26
+#define DIO1_GPIO       33 // Note: not really used on this board
+#define DIO2_GPIO       32 // Note: not really used on this board
