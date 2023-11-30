361ed0a5efde4b6390d7fb8280dbedd69a8dfc67
Joos Van Esbroeck <joos.van.esbroeck@telenet.be>
2023-11-30 09:58:13 +0100

    fix: sleep_ms correction

--- sensor-code/Sensor-LoRa-Sleep/src/configuration.h
+++ sensor-code/Sensor-LoRa-Sleep/src/configuration.h
@@ -7,12 +7,12 @@
 // Configuration
 // -----------------------------------------------------------------------------
 
-#define SERIAL_BAUD      115200                 // Serial debug baud rate
-#define TX_INTERVAL      900                    // Seconds between each transmit
-#define SLEEP_MS         TX_INTERVAL-121 * 1000 // Milliseconds of sleep (= tx_interval - 2 minutes for sensor ramp up, 1 extra second to account for execution delays) 
-#define LORAWAN_PORT     1                      // Port the messages will be sent to
-#define LORAWAN_SF       DR_SF10                // Spreading factor (recommended DR_SF7 for ttn network map purposes, DR_SF10 works for slow moving trackers)
-#define DATA_LENGTH      16                     // Size of TX buffer sent over LoRa
+#define SERIAL_BAUD      115200                     // Serial debug baud rate
+#define TX_INTERVAL      180                        // Seconds between each transmit
+#define SLEEP_MS         (TX_INTERVAL-121) * 1000   // Milliseconds of sleep (= tx_interval - 2 minutes for sensor ramp up, 1 extra second to account for execution delays) 
+#define LORAWAN_PORT     1                          // Port the messages will be sent to
+#define LORAWAN_SF       DR_SF10                    // Spreading factor (recommended DR_SF7 for ttn network map purposes, DR_SF10 works for slow moving trackers)
+#define DATA_LENGTH      16                         // Size of TX buffer sent over LoRa
 
 // -----------------------------------------------------------------------------
 // LoRa SPI
